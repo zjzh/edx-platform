1c6793c10a23aeaf8d02bac8748ab13819288288
Michael Terry <mterry@edx.org>
2020-04-27 15:12:34 -0400

    WIP start date endpoint AA-42

--- openedx/features/calendar_sync/ics.py
+++ openedx/features/calendar_sync/ics.py
@@ -14,9 +14,24 @@ from openedx.core.djangolib.markup import HTML
 from . import get_calendar_event_id
 
 
-def generate_ics_for_event(uid, title, course_name, now, start, organizer_name, organizer_email):
+def get_calendar_event_id(user_id, block_key, date_type, hostname):
     """
-    Generates an ics-formatted bytestring for the given assignment information.
+    Creates a unique event id based on a user and a course block key
+
+    Parameters:
+        user_id (int): A user id context for the calendar event
+        block_key (str): The block key containing the date for the calendar event
+        date_type (str): The type of the date (e.g. 'due', 'start', 'end', etc.)
+        hostname (str): A hostname to namespace this id (e.g. 'open.edx.org')
+    Returns:
+        event id (str)
+    """
+    return '{}.{}.{}@{}'.format(user_id, block_key, date_type, hostname)
+
+
+def generate_ics_for_event(uid, title, description, now, start, organizer_name, organizer_email):
+    """
+    Generates an ics-formatted bytestring for the given event information.
 
     To pretty-print the bytestring, do: `ics.decode('utf8').replace('\r\n', '\n')`
     """
@@ -32,7 +47,7 @@ def generate_ics_for_event(uid, title, course_name, now, start, organizer_name, 
     event.add('dtstamp', now)
     event.add('organizer', organizer, encode=0)
     event.add('summary', title)
-    event.add('description', HTML(_('{assignment} is due for {course}.')).format(assignment=title, course=course_name))
+    event.add('description', description)
     event.add('dtstart', start)
     event.add('duration', timedelta(0))
     event.add('transp', 'TRANSPARENT')  # available, rather than busy
@@ -46,7 +61,7 @@ def generate_ics_for_event(uid, title, course_name, now, start, organizer_name, 
     return cal.to_ical()
 
 
-def generate_ics_for_user_course(course, user, request):
+def generate_ics_for_course_assignments(course, user, request):
     """
     Generates ics-formatted bytestrings of all assignments for a given course and user.
 
@@ -66,8 +81,32 @@ def generate_ics_for_user_course(course, user, request):
             organizer_email=platform_email,
             start=assignment.date,
             title=assignment.title,
-            course_name=course.display_name_with_default,
-            uid=get_calendar_event_id(user, str(assignment.block_key), 'due', request.site.domain),
+            description=HTML(_('{assignment} is due for {course}.')).format(
+                assignment=assignment.title,
+                course=course.display_name_with_default,
+            ),
+            uid=get_calendar_event_id(user.id, str(assignment.block_key), 'due', request.site.domain),
         )
         for assignment in assignments
+    )
+
+
+def generate_ics_for_course_start(course, request):
+    """
+    Generates an ics-formatted bytestring of the start date for a course.
+
+    To pretty-print each bytestring, do: `ics.decode('utf8').replace('\r\n', '\n')`
+    """
+    platform_name = get_value('platform_name', settings.PLATFORM_NAME)
+    platform_email = get_value('email_from_address', settings.DEFAULT_FROM_EMAIL)
+    now = datetime.now(pytz.utc)
+
+    return generate_ics_for_event(
+        now=now,
+        organizer_name=platform_name,
+        organizer_email=platform_email,
+        start=course.start_date,
+        title=_('{course} Begins').format(course=course.display_name_with_default),
+        description='',
+        uid=get_calendar_event_id(0, str(course.id), 'start', request.site.domain),
     )
--- openedx/features/calendar_sync/views/calendar_sync.py
+++ openedx/features/calendar_sync/views/calendar_sync.py
@@ -6,7 +6,7 @@ Views to toggle Calendar Sync settings for a user on a course
 import json
 
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.decorators import method_decorator
@@ -14,11 +14,17 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import View
 from opaque_keys.edx.keys import CourseKey
 from rest_framework import status
+from rest_framework.decorators import action
 
+from lms.djangoapps.courseware.courses import get_course_overview_with_access
 from openedx.features.calendar_sync.api import (
     SUBSCRIBE, UNSUBSCRIBE, subscribe_user_to_calendar, unsubscribe_user_to_calendar
 )
+from openedx.features.calendar_sync.ics import generate_ics_for_course_start
 from util.views import ensure_valid_course_key
+
+MODE_GOOGLE = 'google'
+MODE_ICS = 'ics'
 
 
 class CalendarSyncView(View):
@@ -52,3 +58,41 @@ class CalendarSyncView(View):
             return HttpResponse('Toggle data was not provided or had unknown value.',
                                 status=status.HTTP_422_UNPROCESSABLE_ENTITY)
         return redirect(reverse('openedx.course_experience.course_home', args=[course_id]))
+
+    @action(detail=True)
+    @method_decorator(ensure_valid_course_key)
+    def start(self, request, course_id):
+        """
+        Provides a learner with calendar information for a single "start of course run" event.
+
+        It may be useful to provide a link to this endpoint in a welcome email, for example.
+        Because it may be used in contexts where the user often might not be logged in, but we don't
+        want to add any extra friction and this isn't secret information, we allow anonymous access.
+
+        If the 'mode' query param is passed, it will force a particular mode. Else this endpoint
+        will decide on its own.
+
+        Supported modes:
+         * google: provides a redirect to a google calendar "add event" page
+         * ics: provides a download link for an ics formatted calendar file
+
+        Arguments:
+            request: HTTP request
+            course_id (str): string of a course key
+        """
+        mode = request.query_params.get('mode', MODE_ICS)
+        course_key = CourseKey.from_string(course_id)
+        course_overview = get_course_overview_with_access(request.user, 'see_exists', course_key)
+
+        if mode == MODE_GOOGLE:
+            date = course_overview.start_date.strftime('%Y%m%dT%H%M%SZ')
+            add_event_url = 'https://calendar.google.com/calendar/r/eventedit'
+            add_event_url += '?dates={date}/{date}'.format(date=date)
+            add_event_url += '&text=' + _('{course} Begins').format(course=course_overview.display_name_with_default)
+            add_event_url += '&crm=AVAILABLE'
+            return HttpResponseRedirect(url=add_event_url)
+        elif mode == MODE_ICS:
+            ics = generate_ics_for_course_start(course_overview, request)
+            return HttpResponse(ics, content_type='text/calendar')
+        else:
+            return HttpResponseBadRequest()
