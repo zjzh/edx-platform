c169ea0f63183b0a9c58e870fb30b59a76917ec4
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2021-12-20 19:19:54 +0500

    fix: fixed django4 warnings

--- openedx/core/lib/edx_six.py
+++ openedx/core/lib/edx_six.py
@@ -7,6 +7,6 @@ This is internal and should not be referenced outside of the edx-platform repo.
 
 def get_gettext(o):
     """
-    In python 2 return the ugettext attribute. In python 3 return gettext.
+    In python 2 return the gettext attribute. In python 3 return gettext.
     """
     return o.gettext
