b61ea3d67c5d7ea9e0fe61c020e24b940b242f5c
Julia Eskew <jeskew@edx.org>
2022-01-18 17:11:56 -0500

    SQUASH ME! fix: Adjusted query counts in tests due to new org-level override checking.

--- lms/djangoapps/courseware/tests/test_views.py
+++ lms/djangoapps/courseware/tests/test_views.py
@@ -405,8 +405,8 @@ class IndexQueryTestCase(ModuleStoreTestCase):
     NUM_PROBLEMS = 20
 
     @ddt.data(
-        (ModuleStoreEnum.Type.mongo, 10, 220),
-        (ModuleStoreEnum.Type.split, 4, 205),
+        (ModuleStoreEnum.Type.mongo, 10, 226),
+        (ModuleStoreEnum.Type.split, 4, 213),
     )
     @ddt.unpack
     def test_index_query_counts(self, store_type, expected_mongo_query_count, expected_mysql_query_count):
@@ -1600,8 +1600,8 @@ class ProgressPageTests(ProgressPageBaseTests):
             self.assertContains(resp, "Download Your Certificate")
 
     @ddt.data(
-        (True, 52),
-        (False, 52),
+        (True, 58),
+        (False, 58),
     )
     @ddt.unpack
     def test_progress_queries_paced_courses(self, self_paced, query_count):
@@ -1614,8 +1614,8 @@ class ProgressPageTests(ProgressPageBaseTests):
 
     @patch.dict(settings.FEATURES, {'ASSUME_ZERO_GRADE_IF_ABSENT_FOR_ALL_TESTS': False})
     @ddt.data(
-        (False, 60, 42),
-        (True, 52, 36)
+        (False, 66, 48),
+        (True, 58, 42)
     )
     @ddt.unpack
     def test_progress_queries(self, enable_waffle, initial, subsequent):
