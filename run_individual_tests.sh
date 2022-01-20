ca91d3ba797cd37ed06cb6fa98182953cbb1e9cb
Soban Javed <iamsobanjaved@gmail.com>
2022-01-20 14:29:25 +0500

    run individual modules pytest in GHA

---
+++ run_individual_tests.sh
@@ -0,0 +1,33 @@
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Kindly provide path'
+    exit 0
+fi
+
+test_files=()
+failed_files=()
+
+clear
+
+for arg; do
+    echo "Collecting directories from $arg"
+
+    for file in $(find $arg -name 'test_*.py' -or -name 'tests_*.py' -or -name '*_tests.py' -or -name 'tests.py' -or -type f -path "*/tests/__init__.py") ; do
+        test_files+=("$file")
+    done
+done
+
+echo "Running pytest file-wise on all these files"
+printf "\t%s\n" "${test_files[@]}"
+
+for file in "${test_files[@]}" ; do
+     python -Wd -m pytest --ds="$DJANGO_SETTINGS" -p no:warnings "$file"
+    exit_code=$?
+    if [ $exit_code -ne 0 ] && [ $exit_code -ne 5 ]
+    then
+        failed_files+=("$file")
+    fi
+done
+
+echo "Following files failed during run"
+printf "\t%s\n" "${failed_files[@]}"
